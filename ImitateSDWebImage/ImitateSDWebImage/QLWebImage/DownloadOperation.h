5428f548a1cee3c63cb6d97ff177b0393667c0c6
huangyueqi <1611186361@qq.com>
2016-08-22 20:18:09 +0800

    创建项目

---
+++ ImitateSDWebImage/ImitateSDWebImage/QLWebImage/DownloadOperation.h
@@ -0,0 +1,13 @@
+//
+//  DownloadOperation.h
+//  ImitateSDWebImage
+//
+//  Created by 黄跃奇 on 16/8/22.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DownloadOperation : NSOperation
+
+@end
---
+++ ImitateSDWebImage/ImitateSDWebImage/QLWebImage/DownloadOperation.m
@@ -0,0 +1,13 @@
+//
+//  DownloadOperation.m
+//  ImitateSDWebImage
+//
+//  Created by 黄跃奇 on 16/8/22.
+//  Copyright © 2016年 itheima. All rights reserved.
+//
+
+#import "DownloadOperation.h"
+
+@implementation DownloadOperation
+
+@end
